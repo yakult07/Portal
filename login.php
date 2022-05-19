0e8128e7b489d4326a66ceefccaf1755bb378717
Eduardo Souza <83316377+yakult07@users.noreply.github.com>
2022-05-19 10:49:56 -0300

    add
    add logo,css

--- login.php
+++ login.php
@@ -6,13 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login - Portal Cursos</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-   
+    <link rel="stylesheet" href="css/stylo-login.css">
 
 </head>
 <body>
-    <div class="container">
-        <div class="d-flex justify-content-center">
-
+    <div class="container h-100">
+        <div class="d-flex justify-content-center h-100">
+            <div class="logo_circular">
+                <img src="img/logo.png">
+            </div>
         </div>
     </div>
 </body>
